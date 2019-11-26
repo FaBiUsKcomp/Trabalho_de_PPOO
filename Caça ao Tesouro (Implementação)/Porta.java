8aab9fb91857a214d66f98946f2ef38e06490cfb
FaBiUsKcomp <fabio.oliveira2@computacao.ufla.br>
2019-11-25 21:44:52 -0300

    :hammer: Refatora Classe Porta

--- Caça ao Tesouro (Implementação)/Porta.java
+++ Caça ao Tesouro (Implementação)/Porta.java
@@ -1,26 +1,31 @@
+import java.util.Random;
+
 public class Porta {
     private Comodo destino;
     private Boolean estado;
 
     public Porta (Comodo destino){
         this.destino = destino;
-        estado = true;
+        estado = new Random(2);
     }
 
     public String getSaida(){
         return destino.getNome();
     }
 
-    public String getEstado(){
-        if (estado){
-            return "Funcionando corretamente";
-        } else {
-            return "Emperrada";
-        }
+    public Boolean getEstado(){
+        return estado;
+    }
+
+    public Boolean setEstado(){
+        estado = new Random(2);
+    }
+
+    public String getMensagemPorta(){
+        return getEstado() ? "Funcionando corretamente" : "Emperrada";
     }
 
     public Comodo getDestino(){
         return destino;
     }
-
 }
